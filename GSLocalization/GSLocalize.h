399c37240bd0db10f2c83039ab8c763e0607b553
Gantulga <gangstarmn@gmail.com>
2016-07-01 11:39:43 +0900

    version 0.1.1

--- GSLocalization/GSLocalize.h
+++ GSLocalization/GSLocalize.h
@@ -16,6 +16,7 @@
 // "language" can be (for american english): "en", "en-US", "english". Analogous for other languages.
 #define GSLocalizeSetLanguage(language) [[GSLocalize getInstance] setLanguage:(language)]
 #define GSLocalizeAddBundle(bundleName) [[GSLocalize getInstance] addBundle:(bundleName)]
+#define GSLocalizeCurrentLanguage [[GSLocalize getInstance] currentLanguage]
 
 @interface GSLocalize : NSObject
 
@@ -26,7 +27,7 @@
 #pragma mark - Set Methods;
 
 - (void) setLanguage:(NSString*) lang;
-
+- (NSString *)currentLanguage ;
 #pragma mark - Localization;
 
 - (NSString *)localizedStringForKey:(NSString *)key bundleName:(NSString *)bundleName;
